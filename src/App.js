59646e051affb6f383b9b3ac5bbada2d524b9f35
Frank Diaz <diazfrankp@gmail.com>
2018-12-19 16:16:51 -0500

    Species listed.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import axios from 'axios'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = this.initialState
+    this.state = {
+      animals: []
+    }
   }
 
   componentDidMount() {
-    axios.get('https://localhost:5001/api/animals').then(json => {
-      console.log(json)
+    axios.get('https://localhost:5001/api/animals').then(response => {
+      this.setState({
+        animals: response.data
+      })
     })
   }
 
@@ -20,9 +24,16 @@ class App extends Component {
       <div className="Header">
         <h1>Safari Vacation</h1>
         <h2>What, Where, and How Many</h2>
+        {this.state.animals.map((animal, index) => {
+          return <p key={index}>{animal.species}</p>
+        })}
       </div>
     )
   }
 }
 
 export default App
+
+// .then(json => {
+//   console.log(json)
+// }
